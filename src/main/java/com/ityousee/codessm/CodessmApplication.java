5c4e44c9744f40c243b43a9f60e8541ccdc65a9f
wjh <18137477955@163.com>
2018-05-28 02:17:04 +0800

    dockerfile

---
+++ src/main/java/com/ityousee/codessm/CodessmApplication.java
@@ -0,0 +1,12 @@
+package com.ityousee.codessm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodessmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodessmApplication.class, args);
+    }
+}
