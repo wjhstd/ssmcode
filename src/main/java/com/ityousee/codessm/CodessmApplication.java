b3792a4f27a388dd8a7a700076624fe0c0cf575d
wjh <18137477955@163.com>
2018-05-28 02:32:09 +0800

    dockerfile

--- src/main/java/com/ityousee/codessm/CodessmApplication.java
+++ src/main/java/com/ityousee/codessm/CodessmApplication.java
@@ -2,11 +2,16 @@ package com.ityousee.codessm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
+@RequestMapping("/")
 public class CodessmApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CodessmApplication.class, args);
+    }
+    public String getMessage(){
+        return "hello ityousee";
     }
 }
