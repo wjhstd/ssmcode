fbf5bc281a4c45a14ca29403fb309e4f2386ebb6
wjh <18137477955@163.com>
2018-05-28 18:15:48 +0800

    dockerfile

--- src/main/java/com/ityousee/codessm/CodessmApplication.java
+++ src/main/java/com/ityousee/codessm/CodessmApplication.java
@@ -12,6 +12,6 @@ public class CodessmApplication {
         SpringApplication.run(CodessmApplication.class, args);
     }
     public String getMessage(){
-        return "hello ityousee gg";
+        return "hello ityousee ggs";
     }
 }
